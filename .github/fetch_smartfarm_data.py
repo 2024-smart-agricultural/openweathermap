5d08805065c335a8f9e738d7a7e4bb79870f67d4
in-ae <101018070+in-ae@users.noreply.github.com>
2024-11-25 17:25:45 +0900

    Update fetch_smartfarm_data.py

--- .github/fetch_smartfarm_data.py
+++ .github/fetch_smartfarm_data.py
@@ -96,7 +96,7 @@ def process_data(data):
 
 def save_data(data):
     # JSON 파일로 저장
-    with open("smartfarm_data.json", "w", encoding="utf-8") as f:
+    with open("data/smartfarm_data.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
     print("Data saved successfully!")
